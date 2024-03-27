4e721c82a6b443fafd4eee08ec1dabfc2fa6957e
Yuriy <yd.appdeveloper@gmail.com>
2024-03-27 12:13:32 -0600

    add n Task count functionality

--- Todo List Manager/todoListManager.js
+++ Todo List Manager/todoListManager.js
@@ -1,5 +1,13 @@
 const addForm = document.querySelector(".add");
 const tasks = document.querySelector(".tasks");
+const clearAll = document.querySelector(".clear");
+const messageSpan = document.querySelector(".message span");
+
+function updateMessage() {
+  const textLength = tasks.children.length;
+  messageSpan.textContent = `You have ${textLength} pending tasks`;
+}
+
 
 
 addForm.addEventListener("submit", event => {
@@ -13,7 +21,24 @@ addForm.addEventListener("submit", event => {
                           <i class="bi bi-trash-fill delete"></i>
                         </li>`;
     addForm.reset(); // resetting for new input
+    updateMessage(); // update task numbers
   }
 
 });
 
+tasks.addEventListener("click", event => {
+
+  if (event.target.classList.contains("delete")) {
+    event.target.parentElement.remove();
+    updateMessage();
+  }
+});
+
+clearAll.addEventListener("click", event => {
+  const taskItems = tasks.querySelectorAll("li");
+  taskItems.forEach(item => {
+    item.remove();
+    updateMessage();
+  })
+});
+updateMessage();
