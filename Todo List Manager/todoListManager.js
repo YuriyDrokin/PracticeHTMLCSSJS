f8936681c400af10d08a512b3426103aba06c4cf
Yuriy <yd.appdeveloper@gmail.com>
2024-03-27 11:52:00 -0600

    add Task method to the page

---
+++ Todo List Manager/todoListManager.js
@@ -0,0 +1,19 @@
+const addForm = document.querySelector(".add");
+const tasks = document.querySelector(".tasks");
+
+
+addForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const value = addForm.task.value.trim(); // remove white space 
+
+  if (value.length) {
+
+    tasks.innerHTML += `<li>
+                          <span>${value}</span>
+                          <i class="bi bi-trash-fill delete"></i>
+                        </li>`;
+    addForm.reset(); // resetting for new input
+  }
+
+});
+
