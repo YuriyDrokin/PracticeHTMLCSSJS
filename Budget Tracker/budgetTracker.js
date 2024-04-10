87b7dac7cccf24eaeba3fc8dbc454699fecf59e7
Yuriy <yd.appdeveloper@gmail.com>
2024-04-09 21:57:47 -0600

    Add incomeList and Expense transactions

--- Budget Tracker/budgetTracker.js
+++ Budget Tracker/budgetTracker.js
@@ -1,17 +1,41 @@
 const form = document.querySelector(".add-transaction");
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list");
 let transactions = localStorage.getItem("transactions") !== null ? JSON.parse(localStorage.getItem("transactions")) : [];
+
+
+function generateTemplate(id, source, amount, time) {
+
+  return ` <li data-id="${id}">
+              <p>
+                <span>${source}</span>
+                <span id="time">${time}</span>
+              </p>
+              $<span>${Math.abs(amount)}</span>
+              <i class="bi bi-trash delete"></i>
+            </li>`;
+}
+
+function addTransactionDOM(id, source, amount, time) {
+
+  if (amount > 0) {
+    incomeList.innerHTML += generateTemplate(id, source, amount, time);
+  } else {
+    expenseList.innerHTML += generateTemplate(id, source, amount, time);
+  }
+}
 
 function addTransaction(source, amount) {
   const time = new Date();
   const transaction = {
-
     id: Math.floor(Math.random() * 100000),
     source: source,
     amount: amount,
     time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
   };
-  transactions.push(transactions);
-  localStorage.setItem("transactions", JSON.stringify(transaction));
+  transactions.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  addTransactionDOM(transaction.id, source, amount, transaction.time);
 }
 
 form.addEventListener("submit", event => {
